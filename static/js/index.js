7b49bddb47ae4c6c31f5b6bf9800b67b680fe7d2
icoxfog417 <icoxfog417@yahoo.co.jp>
2018-09-22 15:29:04 +0900

    multi browser support

--- static/js/index.js
+++ static/js/index.js
@@ -1,34 +1,58 @@
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.URL = window.URL || window.webkitURL || window.msURL || window.mozURL;
+
 (function() {
-    var container = document.getElementById("wrap");
     var canvas = document.getElementById("canvas");
     var video = document.getElementById("video");
     var context = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    video.width = window.innerWidth;
+    var height = Math.round(video.width / 4 * 3);
+    canvas.height = height;
+    video.height = height;
+    context = canvas.getContext("2d");
+
+    var ctracker = new clm.tracker();
+    ctracker.init();
+    ctracker.start(video)
+
+    function gumSuccess( stream ) {
+        // add camera stream if getUserMedia succeeded
+        if ("srcObject" in vid) {
+            vid.srcObject = stream;
+        } else {
+            vid.src = (window.URL && window.URL.createObjectURL(stream));
+        }
+        vid.onloadedmetadata = function() {
+            adjustVideoProportions();
+            vid.play();
+        }
+        vid.onresize = function() {
+            adjustVideoProportions();
+            if (trackingStarted) {
+                ctrack.stop();
+                ctrack.reset();
+                ctrack.start(vid);
+            }
+        }
+    }
+
     var media = navigator.mediaDevices.getUserMedia(
         {video: {facingMode: "user"}, audio: false}
     )
     media.then(function(stream){
-        video.srcObject = stream;
+        if ("srcObject" in video) {
+            video.srcObject = stream;
+        } else {
+            video.src = (window.URL && window.URL.createObjectURL(stream));
+        }
+        vidio.onloadedmetadata = function() {
+            vidio.play();
+        }
     }).catch(function(err){
         console.log(err);
     })
-
-    var ctracker = new clm.tracker();
-    ctracker.init();
-
-    function beginTrack(){
-        ctracker.stop();
-        ctracker.reset();
-        canvas.width = container.offsetWidth;
-        canvas.height = container.offsetHeight;
-        video.width = container.offsetWidth;
-        video.height = container.offsetHeight;
-        context = canvas.getContext("2d");
-        ctracker.start(video)
-    }
-
-    window.addEventListener("resize", function() {
-      (!window.requestAnimationFrame) ? setTimeout(beginTrack, 300): window.requestAnimationFrame(beginTrack);
-    });
 
     function drawLoop() {
         requestAnimationFrame(drawLoop);
